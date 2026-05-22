f2f049113b2ec6c3ea421949b3fef6d834021f9b
이성헌 <a39161026@gmail.com>
2026-05-22 15:54:13 +0900

    로그인 회원가입 비밀번호찾기 기능 완성

--- config/settings.py
+++ config/settings.py
@@ -80,3 +80,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 LOGIN_URL = '/accounts/login/'
+PASSWORD_CHANGE_REDIRECT_URL = '/'
+
+# 이메일 설정
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'a39161026@gmail.com'
+EMAIL_HOST_PASSWORD = 'mtwh tagp qydx sztp'
+DEFAULT_FROM_EMAIL = 'a39161026@gmail.com'
