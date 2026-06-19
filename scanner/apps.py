9d18f88f6d368565654799e4a0ff068c3d32c179
이성헌 <a39161026@gmail.com>
2026-06-19 14:48:07 +0900

    media 폴더 중복 자동감시 비활성화

--- scanner/apps.py
+++ scanner/apps.py
@@ -1,21 +1,12 @@
-from django.apps import AppConfig
+﻿from django.apps import AppConfig
 
 
 class ScannerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'scanner'
 
+    # media 폴더 자동 감시 기능은 views.py의 업로드 즉시 스캔과 중복되어
+    # 업로드마다 VT API가 두 번 호출되고, SHA256/엔진 정보가 빠진 중복 ScanLog가
+    # 생성되는 문제가 있어 비활성화함.
     def ready(self):
-        import threading
-        import os
-        from .watcher import start_media_watching
-
-        api_key = os.environ.get('VT_API_KEY', '')
-        if api_key:
-            media_dir = os.path.join(
-                os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'media'
-            )
-            thread = threading.Thread(
-                target=start_media_watching, args=(media_dir, api_key), daemon=True
-            )
-            thread.start()
+        pass
